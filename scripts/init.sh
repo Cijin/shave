9d512eb85edb312e8616495e5c34bf7198ea1f6d
sea gin <cijin@seagin.me>
2025-06-26 09:05:48 +0700

    Update scripts to update frequently updated pkgs

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,8 +5,11 @@ read pkg_name
 go mod edit -module $pkg_name
 
 escaped_pkg_name=$(echo $pkg_name | sed 's/\//\\\//g')
-
 find . -name "*.go" -print0 | xargs -0 sed -i -s "s/shave/${escaped_pkg_name}/g"
+
+go get -u github.com/a-h/templ@latest
+go get -u github.com/clerk/clerk-sdk-go/v2@latest
+go mod tidy
 
 rm -rf .git
 git init
